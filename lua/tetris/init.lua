71f3221fe844ca57e8bb2ee0e706bcde423795d8
Michael Thompson <actionscripted@gmail.com>
2024-01-19 00:16:56 -0500

    feat:basic maps and bufs

--- lua/tetris/init.lua
+++ lua/tetris/init.lua
@@ -15,8 +15,28 @@ local function create_window()
     line = math.floor(((vim.o.lines - height) / 2) - 1),
     minheight = height,
     minwidth = width,
+    noautocmd = true,
     title = "Tetris",
   })
+
+  vim.api.nvim_create_autocmd("BufLeave", {
+    buffer = bufnr,
+    once = true,
+    callback = function()
+      pcall(vim.api.nvim_win_close, tetris_win_id, true)
+      pcall(vim.api.nvim_win_close, win.border.win_id, true)
+      require("telescope.utils").buf_delete(bufnr)
+    end,
+  })
+
+  -- add placeholder lines for score
+  vim.api.nvim_buf_set_lines(bufnr, 0, 0, false, { "Score: 0" })
+
+  -- make buffer read only, no input
+  vim.api.nvim_buf_set_option(bufnr, "modifiable", false)
+
+  -- respond to key presses
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "h", "", { callback = tetris.move_left })
 
   return {
     bufnr = bufnr,
@@ -30,11 +50,15 @@ tetris.close = function()
   vim.api.nvim_win_close(tetris.window.win_id, true)
 end
 
+tetris.move_left = function()
+  print("left")
+end
+
 -- TODO: NO
 -- TODO: also, wtf is this tetris global business
 tetris.run = function()
   tetris.window = create_window()
-  vim.api.nvim_buf_set_keymap(tetris.window.bufnr, "n", "<Esc>", "<Cmd>q<CR>", {})
+  vim.api.nvim_buf_set_keymap(tetris.window.bufnr, "n", "<Esc>", "", { callback = tetris.close })
 end
 
 tetris.setup = function(spec, opts)
